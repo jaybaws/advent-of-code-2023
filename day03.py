995d07b4f68fdc3c142328014e1a12c82bab2d7d
Joris Bastiaans <60797329+jaybaws@users.noreply.github.com>
2023-12-03 22:20:50 +0100

    Pimp!

--- day03.py
+++ day03.py
@@ -1,31 +1,19 @@
+import re
+
 with open("day03_input.txt", "r") as f:
     I = f.readlines()
 
 # All special characters in the schematic
 S = "".join(set("".join(I).replace("\n", "").translate({ord(i): None for i in '0123456789.'})))
 
-
-def part_at(x, y) -> int:
-    middle, left, right = I[y][x], "", ""
-    
-    x_pos = x - 1  # Search left
-    while I[y][x_pos].isdigit():
-        left += I[y][x_pos]
-        x_pos -= 1
-
-    x_pos = x + 1  # Search right
-    while I[y][x_pos].isdigit():
-        right += I[y][x_pos]
-        x_pos += 1
-
-    return int(f"{left[::-1]}{middle}{right}")  # Reverse the left part, as we searched backwards!
-
+def part_at(x: int, y: int) -> int:  # I did learn how to use `re.finditer`` after all!
+    return int([match.group() for match in re.finditer(r'\d+', I[y]) if match.start() <= x <= match.end() ][0])
 
 def gear_ratio(x: int, y: int) -> int:
     parts_found = []
     for y_offset in [-1, 0, 1]:
         for x_offset in [-1, 0, 1]:
-            # My puzzle input has NO '*' chars at the borders, so omitting the try/except clause
+            # My puzzle input has NO '*' directly at any edges of the schematic, so omitting the try/except clause
             s = I[y + y_offset][x + x_offset]
             if s.isdigit():
                 part = part_at(x=x + x_offset, y=y + y_offset)
@@ -37,37 +25,32 @@ def gear_ratio(x: int, y: int) -> int:
     else:
         return 0  # Since we're summing ratio's, this is 'safe' to return 
 
-
 sum_of_valid_parts, sum_of_gear_ratios = 0, 0
 for y, line in enumerate(I):
-    number_str = ""
-    
-    # The 'use' the line-end as a free '.'.
-    # We need a non-digit at the end of the line to stop searching for more digits 
-    # and to startg determining if it's a valid part.  Otherwise, a part-number at
-    # the end of the line simply gets ignored...
-    for x, c in enumerate(line.replace('\n', ".")):
+    current_number = ""
+
+    for x, c in enumerate(line.replace('\n', ".")):  # Add a '.' at the end, so we always hit our stop condition!
         if c.isdigit():
-            number_str += c
-        else:
-            if number_str != "":
+            current_number += c
+        else:  # "stop condition" means, not finding more digits to add...
+            if current_number != "":
                 valid_part_found = False
 
-                for y_offset in [-1, 0, 1]:
-                    for x_offset in range(len(number_str) + 2):
-                        try:  # Because testing array bounds is for p&ssies
+                for y_offset in [-1, 0, 1]:  # Look around the entire part number
+                    for x_offset in range(len(current_number) + 2):
+                        try:  # (because testing array boundaries is for p&ssies...)
                             s = I[y + y_offset][x - x_offset]
-                            if s in S:
+                            if s in S:  # For special characters
                                 valid_part_found = True
                         except IndexError as ie:
-                            pass
+                            pass  # (because fsck 'em!)
 
                 if valid_part_found:
-                    sum_of_valid_parts += int(number_str)
+                    sum_of_valid_parts += int(current_number)
 
-            number_str = ""
+            current_number = ""  # Reset and proceed looking for more part numbers
 
-        if c == "*": # This is only for part 2
+        if c == "*":
             sum_of_gear_ratios += gear_ratio(x, y)
 
 print(f"""
