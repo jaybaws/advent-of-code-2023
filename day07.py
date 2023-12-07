4e76b8e6c67b0076060bc3745641413b592d3872
Joris Bastiaans <60797329+jaybaws@users.noreply.github.com>
2023-12-07 21:27:23 +0100

    woei. klaar nu

--- day07.py
+++ day07.py
@@ -9,44 +9,44 @@ class Hand:
     def score(self, jokers: bool = False):
         V = "J23456789T_QKA" if jokers else "_23456789TJQKA"
         joker_count = self.cards_str.count("J") if jokers else 0
-        hand_score, counts, card_score_str = 0, [], ""
+        counts, score = [], ""
 
         for c in set([*self.cards_str]):
             counts.append(self.cards_str.count(c))
         
         match sorted(counts, reverse=True):
             case [5]:  # Five-of-a-kind
-                hand_score = 7
+                score = "7"
             case [4, 1]:  # Four-of-a-kind
-                hand_score = 6
+                score = "6"
                 match joker_count:
-                    case 1 | 4: hand_score = 7
+                    case 1 | 4: score = "7"
             case [3, 2]:  # Full-house
-                hand_score = 5
+                score = "5"
                 match joker_count:
-                    case 2 | 3: hand_score = 7
+                    case 2 | 3: score = "7"
             case [3, 1, 1]:  # Three-of-a-kind
-                hand_score = 4
+                score = "4"
                 match joker_count:
-                    case 1 | 3: hand_score = 6
+                    case 1 | 3: score = "6"
             case [2, 2, 1]:  # Two-pair
-                hand_score = 3
+                score = "3"
                 match joker_count:
-                    case 1: hand_score = 5
-                    case 2: hand_score = 6
+                    case 1: score = "5"
+                    case 2: score = "6"
             case [2, 1, 1, 1]:  # One-pair
-                hand_score = 2
+                score = "2"
                 match joker_count:
-                    case 1 | 2: hand_score = 4
+                    case 1 | 2: score = "4"
             case [1, 1, 1, 1, 1]:  # High-card
-                hand_score = 1
+                score = "1"
                 match joker_count:
-                    case 1: hand_score = 2
+                    case 1: score = "2"
 
         for c in [*self.cards_str]:
-            card_score_str += str(card_value(c, V)).zfill(2)
+            score += str(card_value(c, V)).zfill(2)
 
-        return (hand_score, int(card_score_str), int(self.bid_str))
+        return (int(score), int(self.bid_str))
 
 hands: List[Hand] = []
 with open("day07_input.txt", "r") as f:
