a767cc648866f443af31e75467e2e12d631cf701
Joris Bastiaans <60797329+jaybaws@users.noreply.github.com>
2023-12-11 12:15:29 +0100

    Pimp

--- day11.py
+++ day11.py
@@ -1,16 +1,14 @@
-with open("day11_input.txt", "r") as f: C = [ [*l] for l in f.read().splitlines() ]
-galaxies = [ (r,c) for r in range(len(C)) for c in range(len(C[0])) if C[r][c] == "#" ]
-empty_rows = [ idx for idx, row in enumerate(C) if "#" not in row ]
-empty_cols = [ ci for ci in range(len(C[0])) if "#" not in [ r[ci] for r in C ] ]
+with open("day11_input.txt", "r") as f: IMAGE = [ [*line] for line in f.read().splitlines() ]
+galaxies = [ (r, c) for r in range(len(IMAGE)) for c in range(len(IMAGE[0])) if IMAGE[r][c] == "#" ]
+empty_rows = [ row_index for row_index, row in enumerate(IMAGE) if "#" not in row ]
+empty_cols = [ col_index for col_index in range(len(IMAGE[0])) if "#" not in [ r[col_index] for r in IMAGE ] ]
 
 ans1 = ans2 = 0
-for ia, (ga_r, ga_c) in enumerate(galaxies):
-    for ib, (gb_r, gb_c) in enumerate(galaxies):
-        if ib > ia:
-            d = abs(gb_r - ga_r) + abs(gb_c - ga_c)
-            e = len([c for c in empty_cols if min(ga_c, gb_c) < c < max(ga_c, gb_c)]) + len([r for r in empty_rows if min(ga_r, gb_r) < r < max(ga_r, gb_r)])
-            ans1 += d + e
-            ans2 += d + ((1000000-1) * e)
+for ((ar, ac), (br, bc)) in [ ((a_r, a_c), (b_r, b_c)) for ia, (a_r, a_c) in enumerate(galaxies) for ib, (b_r, b_c) in enumerate(galaxies) if ib > ia]:
+    manhattan_distance = abs(br - ar) + abs(bc - ac)
+    expanded_space = len([c for c in empty_cols if min(ac, bc) < c < max(ac, bc)]) + len([r for r in empty_rows if min(ar, br) < r < max(ar, br)])
+    ans1 += manhattan_distance + ((2-1) * expanded_space)
+    ans2 += manhattan_distance + ((1000000-1) * expanded_space)
 
 print(f"""
 *** ANSWERS:
