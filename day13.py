54b3af7a305277bed7c4f9879ea1a516ad92607b
Joris Bastiaans <60797329+jaybaws@users.noreply.github.com>
2023-12-13 13:56:51 +0100

    Pimp

--- day13.py
+++ day13.py
@@ -1,39 +1,26 @@
 import numpy as np
 
-ans1 = ans2 = 0
-M = []
+Y_AXIS, X_AXIS, M = 0, 1, []
 
 with open("day13_input.txt", "r") as f:
     for map in f.read().split("\n\n"):
         M.append([ [*mapline] for mapline in map.split() ])
 
+ans1 = ans2 = 0
 for m in M:
     map = np.array(m)
-    H, W = len(map), len(map[0])
-
-    for col in range(1, W):
-        b = min(col, W-col)
-        slice = map[0:, col-b:col+b]
-        left, right = np.hsplit(slice, 2)
-        right = np.fliplr(right)
-        comparison = left == right
-        num_diff = len([ 1 for y in comparison for x in y if not x ])
-        if num_diff == 0:
-            ans1 += col
-        elif num_diff == 1:
-            ans2 += col
-
-    for row in range(1, H):
-        d = min(row, H-row)
-        slice = map[row-d:row+d, 0:]
-        up, down = np.vsplit(slice, 2)
-        down = np.flipud(down)
-        comparison = up == down
-        num_diff = len([ 1 for y in comparison for x in y if not x ])
-        if num_diff == 0:
-            ans1 += 100 * row
-        elif num_diff == 1:
-            ans2 += 100 * row
+    for D, axis, factor in [(len(map[0]), X_AXIS, 1), (len(map), Y_AXIS, 100)]:
+        for i in range(1, D):
+            d = min(i, D-i)
+            slice = map[0:, i-d:i+d] if axis==X_AXIS else map[i-d:i+d, 0:]
+            p, q = np.split(slice, 2, axis)
+            q = np.flip(q, axis)
+            comp = p == q
+            num_diff = len([ 1 for y in comp for x in y if not x ])
+            if num_diff == 0:
+                ans1 += factor * i
+            elif num_diff == 1:
+                ans2 += factor * i
 
 print(f"""
 *** ANSWERS:
