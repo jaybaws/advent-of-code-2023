32ad3bf3d974db065d041886f39d2dcab8c10770
Joris Bastiaans <60797329+jaybaws@users.noreply.github.com>
2023-12-17 01:00:43 +0100

    Pimp

--- day16.py
+++ day16.py
@@ -1,12 +1,13 @@
 from collections import deque
+from typing import Tuple
 
 with open("day16_input.txt", "r") as f:
     M = [ [*line.strip()] for line in f.read().splitlines() ]
     H = len(M)
     W = len(M[0])
 
-def energy(x, y, dx, dy) -> int:
-    tiles_to_consider = deque([ ((x, y), (dx, dy)) ])
+def energy(start: Tuple[int, int], heading: Tuple[int, int]) -> int:
+    tiles_to_consider = deque([ (start, heading) ])
     tiles_seen = set()
 
     while tiles_to_consider:
@@ -15,9 +16,8 @@ def energy(x, y, dx, dy) -> int:
 
         if 0 <= x < W and 0 <= y < H:
             s = M[y][x]
-            if s == "." or (s == "-" and dx != 0) or (s == "|" and dy != 0):
-                pass
-            elif s == "/":
+            
+            if s == "/":
                 dy, dx = -dx, -dy
             elif s == "\\":
                 dy, dx = dx, dy
@@ -27,21 +27,21 @@ def energy(x, y, dx, dy) -> int:
                 match s:
                     case "-":
                         tiles_to_consider.append(((x, y), (-1, 0)))
-                        tiles_to_consider.append(((x, y), (1, 0)))
+                        tiles_to_consider.append(((x, y), (+1, 0)))
                     case "|":
                         tiles_to_consider.append(((x, y), (0, -1)))
-                        tiles_to_consider.append(((x, y), (0, 1)))
+                        tiles_to_consider.append(((x, y), (0, +1)))
                     case _:
                         tiles_to_consider.append(((x, y), (dx, dy)))
 
-    return len(set([ (x, y) for ((x, y), (_, _)) in tiles_seen ]))
+    return len(set([ (x, y) for ((x, y), _) in tiles_seen ]))
 
-ans1 = energy(-1, 0, 1, 0)
+ans1 = energy((-1, 0), (+1, 0))
 
 ans2 = 0
 for x in range(W):
-    ans2 = max(ans2, energy(x, -1, 0, 1), energy(x, H, 0, -1))
+    ans2 = max(ans2, energy((x, -1), (0, +1)), energy((x, H), (0, -1)))
 for y in range(H):
-    ans2 = max(ans2, energy(-1, y, 1, 0), energy(W, y, -1, 0))
+    ans2 = max(ans2, energy((-1, y), (+1, 0)), energy((W, y), (-1, 0)))
 
 print(f"ANSWERS -> part1=({ans1}) part2=({ans2}).")
